584eaa3d44e5a1cc47003534b29289f1468cc5e6
zextras_matteodalzovo <matteo.dalzovo@zextras.com>
2020-03-10 16:17:03 +0100

    created FileLoaderComponent

---
+++ zapp-ui/src/components/primitive/FileLoader.jsx
@@ -0,0 +1,63 @@
+
+import React, { useRef } from 'react';
+import styled from "styled-components";
+import PropTypes from 'prop-types';
+import IconButton from "./IconButton";
+import defaultTheme from "../../theme/Theme";
+const FileInput = styled.input`
+display: none;
+`;
+
+const FileLoader = ({
+	onChange,
+	icon,
+	iconColor,
+	multiple,
+	accept,
+	backgroundColor,
+	size
+}) => {
+	const inputRef = useRef();
+
+	return (
+		<>
+			<FileInput
+				type="file"
+				ref={inputRef}
+				onChange={(ev) => onChange(ev, inputRef.current.files)}
+				multiple={multiple}
+				accept={accept}
+			/>
+			<IconButton
+				icon={icon}
+				iconColor={iconColor}
+				size={size}
+				backgroundColor={backgroundColor}
+				onClick={() => inputRef.current && inputRef.current.click()}
+			/>
+		</>
+	)
+};
+
+FileLoader.propTypes = {
+	onChange: PropTypes.func,
+	/** Color of the icon */
+	iconColor: IconButton.propTypes.iconColor,
+	/** Color of the button */
+	backgroundColor: IconButton.propTypes.backgroundColor,
+	/** button size */
+	size: IconButton.propTypes.size,
+	/** icon name */
+	icon: IconButton.propTypes.icon,
+	/** accept multiple files */
+	multiple: PropTypes.bool,
+	/** capture mode (see <input type="file"> docs) */
+	capture: PropTypes.string,
+	accept: PropTypes.string
+};
+
+FileLoader.defaultProps = {
+	icon: 'Attach'
+};
+
+export default FileLoader;

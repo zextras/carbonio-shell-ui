f7eb10089f868ac75edc2e106119c650e4078b44
zextras_matteodalzovo <matteo.dalzovo@zextras.com>
2020-10-14 11:48:56 +0200

    ignored ts error

--- src/app/app-loader.ts
+++ src/app/app-loader.ts
@@ -24,6 +24,8 @@ import * as ReactI18n from 'react-i18next';
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import * as ZappUI from '@zextras/zapp-ui';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
 import RichTextEditor from '../../zapp-ui/src/components/inputs/RichTextEditor';
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore

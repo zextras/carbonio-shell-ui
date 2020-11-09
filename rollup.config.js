3b89895ab561026283775e81331a21b4cfefc687
Michele Olivo <michele.olivo@zextras.com>
2020-11-09 14:22:18 +0100

    Added `network.soapFetch` mock for testing with Jest.

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2020 Zextras
+ *
+ *  The contents of this file are subject to the Zextras EULA;
+ * you may not use this file except in compliance with the EULA.
+ * You may obtain a copy of the EULA at
+ * http://www.zextras.com/zextras-eula.html
+ * *** END LICENSE BLOCK *****
+ */
+
+export default {
+	input: 'src/jest-mocks.js',
+	output: {
+		file: 'build/jest-mocks.js',
+		format: 'cjs'
+	},
+	external: [
+		'node-fetch'
+	]
+};

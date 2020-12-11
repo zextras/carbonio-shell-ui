525dcc5c451103b185629922b3e811a29a5b4330
Zextras SRL <packages@zextras.com>
2020-12-11 19:00:40 +0100

    Trial with changelog generation fo rpm & deb

--- build-pkgs.sh
+++ build-pkgs.sh
@@ -1,47 +1,150 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "$0: usage: build-pkgs.sh <zapp_component> <version>"
-    exit 1
-fi
+set_pkgs_author() {
+  git config user.name "Zextras SRL"
+  git config user.email "packages@zextras.com"
+}
 
-name=$1
-version=$2
+retrieve_previous_tag() {
+  local previous_tag
+  if [ ! -f /usr/bin/git ]; then
+    install_deps
+  fi
+  previous_tag="$(git describe --abbrev=0 "$(git describe --abbrev=0 --tags)^")"
+  printf "%s" "${previous_tag}"
+}
 
-mkdir artifacts
+bump_deb() {
+  local previous_version="${1}"
+  local current_version="${2}"
+  local branch="${3}"
 
-# Download changelog utility
-curl -O -L -C -  https://github.com/M0Rf30/changelog/releases/download/0.1.0/changelog
-chmod +x changelog
+  local argline="--debian-branch $branch --git-author"
 
-# Init changelog skeleton
-./changelog init --author "Zextras SRL" --email "packages@zextras.com" --since "$version"
-./changelog prepare --author "Zextras SRL" --email "packages@zextras.com"
-./changelog debian --vars='{"name":"zextras-zapp-shell"}'
+  if [[ "${branch}" =~ "beta" ]]; then
+    argline+=" -D unstable"
+  else
+    argline+=" -D stable"
+  fi
 
-if [ -f /etc/redhat-release ]; then
-  ## Set the yum proxy
-  sed -i"" '2iproxy=http://aptproxy.local.zextras.com:3142' /etc/yum.conf
+  gbp dch ${argline} \
+    --since ${previous_version} \
+    --new-version ${current_version} \
+    --git-author
+}
 
-  yum install -y rpm-build
-  mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+bump_rpm() {
+  local previous_version="${1}"
+  local current_version="${2}"
+  local branch="${3}"
+  local name="${4}"
+
+  local argline="--packaging-branch $branch \
+    --git-author \
+    --spec-file=zextras-zapp-$name.spec \
+    --spawn-editor=0"
+
+  local valid_rel=${current_version%*.beta*}b${current_version##*.}
+  
+  gbp rpm-ch ${argline} \
+    --since ${previous_version} \
+    --changelog-revision ${valid_rel}
+
+  sed -i -e "s/Version:\([ ]*\).*/Version:\1${valid_rel}/g" zextras-zapp-$name.spec
+}
+
+install_deps() {
+  if [ -f /etc/redhat-release ]; then
+
+    ## Set the yum proxy
+    #sed -i"" '2iproxy=http://aptproxy.local.zextras.com:3142' \
+    /etc/yum.conf
+
+    yum install -y rpm-build
+    mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+
+  else
+
+    #Set the apt HTTP proxy
+    # echo 'Acquire::HTTP::Proxy "http://aptproxy.local.studiostorti.com:3142";' >/etc/apt/apt.conf.d/01proxy
+    # echo 'Acquire::HTTPS::Proxy "false";' >>/etc/apt/apt.conf.d/01proxy
+
+    apt-get update
+    apt-get install -y --no-install-recommends \
+      build-essential \
+      cdbs \
+      devscripts \
+      fakeroot \
+      git-buildpackage \
+      git-buildpackage-rpm \
+      libdistro-info-perl
+
+  fi
+}
+
+bump() {
+  local previous_version="${1}"
+  local current_version="${2}"
+  local branch="${3}"
+  local name="${4}"
+  bump_deb "${previous_version}" "${current_version#v}" "${branch}"
+  bump_rpm "${previous_version}" "${current_version#v}" "${branch}" "${name}"
+}
+
+prepare() {
+  if [ ! -d artifacts ]; then
+    mkdir artifacts
+  fi
+}
+
+build_rpm() {
   mv "pkg/com_zextras_zapp_$name.zip" /root/rpmbuild/SOURCES/
   rpmbuild -ba "zextras-zapp-$name.spec" || exit 1
   mv /root/rpmbuild/RPMS/*/*.rpm artifacts
-else
-  # Set the apt HTTP proxy
-  echo 'Acquire::HTTP::Proxy "http://aptproxy.local.studiostorti.com:3142";' >/etc/apt/apt.conf.d/01proxy
-  echo 'Acquire::HTTPS::Proxy "false";' >>/etc/apt/apt.conf.d/01proxy
+}
 
-  apt-get update
-  apt-get install -y --no-install-recommends \
-    build-essential \
-    cdbs \
-    devscripts \
-    equivs \
-    fakeroot
+build_deb() {
   mv "pkg/com_zextras_zapp_$name.zip" .
   dpkg-buildpackage -b || exit 1
   mv ../*.deb artifacts
-fi
+}
 
+build() {
+  prepare
+  set_pkgs_author
+  install_deps
+  if [ -f /etc/redhat-release ]; then
+    build_rpm
+  else
+    build_deb
+  fi
+}
+
+usage() {
+  echo "${0}: usage: ./build-pkgs.sh build <zapp_component>"
+  echo "${0}:        ./build-pkgs.sh bump <current_version> <branch> <zapp_component>"
+  exit 1
+}
+
+# Script entrypoint
+process_args() {
+
+  case "$1" in
+    bump)
+      local current_version="${2}"
+      local branch=${3}
+      local name=${4}
+      local previous_version
+      previous_version="$(retrieve_previous_tag)"
+
+      bump "${previous_version}" "${current_version}" "${branch}" "${name}"
+      ;;
+    build)
+      local name="${2}"
+      build "${name}"
+      ;;
+    *) usage ;;
+  esac
+}
+
+process_args "$@"

f4a2f5c92611081d0d7c500b95ad31caf2c46c63
Michele Olivo <michele.olivo@zextras.com>
2021-01-29 17:26:25 +0100

    ci: updated documentation build settings

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -7,88 +7,89 @@
 
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
+const { zapp, description } = require('../../package.json');
 
 const siteConfig = {
-  title: 'Zextras Shell', // Title for your website.
-  tagline: '',
-  url: 'https://doc.dev.zextras.com', // Your website URL
-  baseUrl: '/iris/zapp-shell/' + (process.env.BRANCH_NAME ?  `${process.env.BRANCH_NAME}/` : ''), // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+	title: 'Zextras Shell', // Title for your website.
+	tagline: description,
+	url: 'https://doc.dev.zextras.com', // Your website URL
+	baseUrl: `/iris/${zapp.packagename}/${  process.env.BRANCH_NAME ?  `${process.env.BRANCH_NAME}/` : ''}`, // Base URL for your project */
+	// For github.io type URLs, you would set the url and baseUrl like:
+	//   url: 'https://facebook.github.io',
+	//   baseUrl: '/test-site/',
 
-  // Used for publishing and more
-  projectName: 'com_zextras_zapp_shell',
-  organizationName: 'Zextras',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+	// Used for publishing and more
+	projectName: zapp.packagename,
+	organizationName: 'Zextras',
+	// For top-level user or org sites, the organization is still the same.
+	// e.g., for the https://JoelMarcey.github.io site, it would be set like...
+	//   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'main', label: 'Docs'},
-    {doc: 'CHANGELOG', label: 'Change Log'},
-    {page: 'help', label: 'Help'}
-  ],
+	// For no header links in the top nav bar -> headerLinks: [],
+	headerLinks: [
+		{doc: 'main', label: 'Docs'},
+		{doc: 'CHANGELOG', label: 'Change Log'},
+		{page: 'help', label: 'Help'}
+	],
 
-  /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+	/* path to images for header/footer */
+	headerIcon: 'img/favicon.ico',
+	footerIcon: 'img/favicon.ico',
+	favicon: 'img/favicon.ico',
 
-  /* Colors for website */
-  colors: {
-    primaryColor: '#149c6f',
-    secondaryColor: '#0e6d4d',
-  },
+	/* Colors for website */
+	colors: {
+		primaryColor: '#149c6f',
+		secondaryColor: '#0e6d4d',
+	},
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
+	/* Custom fonts for website */
+	/*
+	fonts: {
+		myFont: [
+			"Times New Roman",
+			"Serif"
+		],
+		myOtherFont: [
+			"-apple-system",
+			"system-ui"
+		]
+	},
+	*/
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © 2019-${new Date().getFullYear()} Zextras`,
+	// This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+	copyright: `Copyright © 2019-${new Date().getFullYear()} Zextras`,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
+	highlight: {
+		// Highlight.js theme to use for syntax highlighting in code blocks.
+		theme: 'default',
+	},
 
-  // Add custom scripts here that would be placed in <script> tags.
-  // scripts: ['https://buttons.github.io/buttons.js'],
+	// Add custom scripts here that would be placed in <script> tags.
+	// scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
+	// On page navigation for the current documentation page.
+	onPageNav: 'separate',
+	// No .html extensions for paths.
+	cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  // ogImage: 'img/undraw_online.svg',
-  // twitterImage: 'img/undraw_tweetstorm.svg',
+	// Open Graph and Twitter card images.
+	// ogImage: 'img/undraw_online.svg',
+	// twitterImage: 'img/undraw_tweetstorm.svg',
 
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+	// For sites with a sizable amount of content, set collapsible to true.
+	// Expand/collapse the links and subcategories under categories.
+	// docsSideNavCollapsible: true,
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+	// Show documentation's last contributor's name.
+	// enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+	// Show documentation's last update time.
+	// enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+	// You may provide arbitrary config keys to be used as needed by your
+	// template. For example, if you need your repo's URL...
+	//   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;

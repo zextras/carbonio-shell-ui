099f1257af2c5111709d7cb86b2078016155c9d6
Mattteo Dal Zovo <zovomat@gmail.com>
2021-02-23 20:43:27 +0100

    feat: added automatic routing to the highest-priority app

--- src/shell/boards/boards-router-container.jsx
+++ src/shell/boards/boards-router-container.jsx
@@ -10,9 +10,9 @@
  */
 
 import React, { Suspense, useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import { reduce } from 'lodash';
+import { reduce, forEach, minBy, filter } from 'lodash';
 import { combineLatest } from 'rxjs';
 import { map as rxMap } from 'rxjs/operators';
 import { Container } from '@zextras/zapp-ui';
@@ -29,56 +29,98 @@ const _BoardsRouterContainer = styled(Container)`
 `;
 
 export default function BoardsRouterContainer() {
-	const { cache } = useAppsCache();
+	const { cache, loaded } = useAppsCache();
 	const [routes, setRoutes] = useState([]);
-
+	const [mainRoute, setMainRoute] = useState();
+	const history = useHistory();
 	useEffect(() => {
-		const subscription = combineLatest(
-			reduce(
-				cache,
-				(acc, app) => {
-					acc.push(
-						app.routes.pipe(
-							rxMap((appRoutes) => ({ appRoutes, app }))
+		let timedRouting;
+		if (loaded && history.location.pathname === '/' && mainRoute) {
+			timedRouting = setTimeout(() => history.push(mainRoute), 500);
+		}
+		return () => {
+			if (timedRouting) {
+				clearTimeout(timedRouting);
+			}
+		}
+	}, [history, loaded, mainRoute])
+	useEffect(() => {
+		const subscriptions = [
+			combineLatest(
+				reduce(
+					cache,
+					(acc, app) => {
+						acc.push(
+							app.routes.pipe(
+								rxMap((appRoutes) => ({ appRoutes, app }))
+							)
+						);
+						return acc;
+					},
+					[]
+				)
+			)
+				.subscribe((allAppRoutes) => {
+					setRoutes(
+						reduce(
+							allAppRoutes,
+							(acc, { appRoutes, app }) => {
+								reduce(
+									appRoutes,
+									(r, appRoute) => {
+										const RouteView = appRoute.view;
+										r.push(
+											<Route key={`${app.pkg.package}|${appRoute.route}`} exact path={`/${app.pkg.package}${appRoute.route}`}>
+												<Suspense fallback={<LoadingView />}>
+													<AppContextProvider key={app.pkg.package} pkg={app.pkg}>
+														<RouteView />
+													</AppContextProvider>
+												</Suspense>
+											</Route>
+										);
+										return r;
+									},
+									acc
+								);
+								return acc;
+							},
+							[]
 						)
 					);
-					return acc;
-				},
-				[]
+				}),
+			combineLatest(
+				reduce(
+					cache,
+					(acc, app) => {
+						acc.push(
+							app.mainMenuItems.pipe(
+								rxMap((mainRoutes) => ({ mainRoutes, app }))
+							)
+						);
+						return acc;
+					},
+					[]
+				)
 			)
-		)
-			.subscribe((allAppRoutes) => {
-				setRoutes(
-					reduce(
-						allAppRoutes,
-						(acc, { appRoutes, app }) => {
-							reduce(
-								appRoutes,
-								(r, appRoute) => {
-									const RouteView = appRoute.view;
-									r.push(
-										<Route key={`${app.pkg.package}|${appRoute.route}`} exact path={`/${app.pkg.package}${appRoute.route}`}>
-											<Suspense fallback={<LoadingView />}>
-												<AppContextProvider key={app.pkg.package} pkg={app.pkg}>
-													<RouteView />
-												</AppContextProvider>
-											</Suspense>
-										</Route>
-									);
-									return r;
-								},
-								acc
-							);
-							return acc;
-						},
-						[]
-					)
-				);
-			});
-
+				.subscribe((allMainRoutes) => {
+					const fastest = minBy(
+						filter(
+							allMainRoutes,
+							({ mainRoutes }) => mainRoutes.length > 0
+						),
+						({ app }) => app.pkg.priority
+					);
+					if (fastest) {
+						const route = `${fastest?.app?.pkg?.package}${fastest?.mainRoutes?.[0].to}`;
+						setMainRoute(
+							route
+						);
+					}
+				}),
+		];
 		return () => {
-			if (subscription) {
-				subscription.unsubscribe();
+			if (subscriptions && subscriptions.length > 0) {
+				forEach(subscriptions, (s) => s.unsubscribe());
 			}
 		};
 	}, [cache]);

1f39f9abeb1b7f9fed85d4300890d5c0f1e126f1
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2021-06-14 08:34:40 +0200

    feat: added copy to clipboard function

--- src/settings/module-version-settings.tsx
+++ src/settings/module-version-settings.tsx
@@ -1,3 +1,14 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2011-2020 Zextras
+ *
+ *  The contents of this file are subject to the Zextras EULA;
+ * you may not use this file except in compliance with the EULA.
+ * You may obtain a copy of the EULA at
+ * http://www.zextras.com/zextras-eula.html
+ * *** END LICENSE BLOCK *****
+ */
+
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FormSubSection, Container, Text, Badge, Divider, Tooltip } from '@zextras/zapp-ui';
@@ -11,6 +22,11 @@ const ModuleVersionSettings: FC<{
 	const [t] = useTranslation();
 	const accounts = useUserAccounts();
 
+	const copyToClipboard: any = (e: any) => {
+		e.preventDefault();
+		navigator.clipboard.writeText(e.target.innerText);
+	};
+
 	return (
 		<>
 			<FormSubSection
@@ -23,7 +39,9 @@ const ModuleVersionSettings: FC<{
 						<Container orientation="horizontal" mainAlignment="space-between">
 							<Text>{item.name}</Text>
 							<Tooltip placement="top" label="Click to copy">
-								<Text style={{ cursor: 'pointer' }}>Version: {item.version}</Text>
+								<Text style={{ cursor: 'pointer' }} onClick={(e: any): any => copyToClipboard(e)}>
+									Version: {item.version}
+								</Text>
 							</Tooltip>
 						</Container>
 						<Container

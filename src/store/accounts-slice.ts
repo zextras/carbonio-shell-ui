b94970c4541ced08d79d3c7ff75484680dd5b191
abhishek_zextras <abhishek.kumar@zextras.com>
2021-06-22 13:11:39 +0200

    feat: permission settings soap request  added

--- src/store/accounts-slice.ts
+++ src/store/accounts-slice.ts
@@ -36,6 +36,7 @@ type DoLoginArgs = {
 export type ModifyPrefsArgs = {
 	props: Record<string, { value: any; app: string }>;
 	prefs: Record<string, any>;
+	permissions: Record<string, any>;
 };
 
 type NormalizeAccountParams = {
@@ -294,6 +295,77 @@ export const modifyPrefs = createAsyncThunk<any, ModifyPrefsArgs>(
 			// 	}
 			// ];
 		}
+		if (mods.permissions) {
+			requests.ModifyPermissionRequest = `<BatchRequest xmlns="urn:zimbra" onerror="stop"><RevokeRightsRequest xmlns="urn:zimbraAccount" requestId="0">${
+				mods.permissions.freeBusy
+					? map(mods.permissions.freeBusy.current, (right) => {
+							if (right.gt === 'dom')
+								return `<ace right="viewFreeBusy" gt="${right.gt}" zid="${right.zid}" d="zextras.com"/>`;
+							if (right.gt === 'all' && right.deny)
+								return `<ace right="viewFreeBusy" gt="${right.gt}" zid="${right.zid}" deny="1"/>`;
+							if (right.gt === 'usr')
+								return `<ace right="viewFreeBusy" gt="${right.gt}" zid="${right.zid}" d="${right.d}"/>`;
+							return `<ace right="viewFreeBusy" gt="${right.gt}" zid="${right.zid}" />`;
+					  }).join('')
+					: ''
+			}${
+				mods.permissions.inviteRight
+					? map(mods.permissions.inviteRight.current, (right) => {
+							if (right.gt === 'all' && right.deny)
+								return `<ace right="invite" gt="${right.gt}" zid="${right.zid}" deny="1"/>`;
+							if (right.gt === 'usr')
+								return `<ace right="invite" gt="${right.gt}" zid="${right.zid}" d="${right.d}"/>`;
+							return `<ace right="invite" gt="${right.gt}" zid="${right.zid}" />`;
+					  }).join('')
+					: ''
+			}</RevokeRightsRequest><GrantRightsRequest xmlns="urn:zimbraAccount" requestId="1">${
+				mods.permissions.freeBusy
+					? `${((): string => {
+							if (mods.permissions.freeBusy.new.gt === 'dom') {
+								return `<ace right="viewFreeBusy" gt="${mods.permissions.freeBusy.new.gt}" d="zextras.com"/>`;
+							}
+							if (
+								mods.permissions.freeBusy.new.gt === 'all' &&
+								mods.permissions.freeBusy.new.deny
+							) {
+								return `<ace right="viewFreeBusy" gt="${mods.permissions.freeBusy.new.gt}" deny=1/>`;
+							}
+							if (mods.permissions.freeBusy.new.gt === 'usr') {
+								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+								// @ts-ignore
+								return map(
+									mods.permissions.freeBusy.new.d,
+									(u) =>
+										`<ace right="viewFreeBusy" gt="${mods.permissions.freeBusy.new.gt}" d="${u.email}"/>`
+								).join('');
+							}
+							return `<ace right="viewFreeBusy" gt="${mods.permissions.freeBusy.new.gt}" />`;
+					  })()}`
+					: ''
+			}${
+				mods.permissions.inviteRight
+					? `${((): string => {
+							if (
+								mods.permissions.inviteRight.new.gt === 'all' &&
+								mods.permissions.inviteRight.new.deny
+							) {
+								return `<ace right="invite" gt="${mods.permissions.inviteRight.new.gt}" deny=1/>`;
+							}
+							if (mods.permissions.inviteRight.new.gt === 'usr') {
+								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+								// @ts-ignore
+								return map(
+									mods.permissions.inviteRight.new.d,
+									(u) =>
+										`<ace right="invite" gt="${mods.permissions.inviteRight.new.gt}" d="${u.email}"/>`
+								).join('');
+							}
+							return `<ace right="invite" gt="${mods.permissions.inviteRight.new.gt}" />`;
+					  })()}`
+					: ''
+			}
+			</GrantRightsRequest></BatchRequest>`;
+		}
 		const res = await fetch('/service/soap/BatchRequest', {
 			method: 'POST',
 			headers: {
@@ -311,6 +383,7 @@ export const modifyPrefs = createAsyncThunk<any, ModifyPrefsArgs>(
 				<soap:Body>
 					${requests.ModifyPrefsRequest ?? ''}
 					${requests.ModifyPropertiesRequest ?? ''}
+					${requests.ModifyPermissionRequest ?? ''}
 				</soap:Body>
 			</soap:Envelope>
 		`

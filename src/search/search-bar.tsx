3c37034f4ebbc965fba02399df861c1e1a268648
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2021-11-05 10:16:07 +0100

    fix: tentative fix to search bug

--- src/search/search-bar.tsx
+++ src/search/search-bar.tsx
@@ -262,12 +262,12 @@ export const SearchBar: FC<SearchBarProps> = ({ currentApp, primaryAction, secon
 		(newVal) => {
 			setModuleSelection(find(moduleSelectorItems, (item) => item.value === newVal));
 			setInputState([]);
-			updateQuery([]);
+			// updateQuery([]);
 			if (currentApp === SEARCH_APP_ID) {
-				// history.push(`/${SEARCH_APP_ID}/${newVal}`);
+				history.push(`/${SEARCH_APP_ID}/${newVal}`);
 			}
 		},
-		[currentApp, moduleSelectorItems, updateQuery]
+		[currentApp, history, moduleSelectorItems]
 	);
 	const [triggerSearch, setTriggerSearch] = useState(false);
 	const containerRef = useRef<HTMLDivElement>();

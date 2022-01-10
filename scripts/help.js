006c4614f36ceca76017fecaa217ffc278246731
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2022-01-10 09:56:25 +0100

    Squashed 'sdk/' content from commit 23afff0
    git-subtree-dir: sdk
    git-subtree-split: 23afff01231d23c60253dcf7edc96593d7a8f9ec

---
+++ scripts/help.js
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+const chalk = require('chalk');
+
+exports.runHelp = async () => {
+	console.log(chalk.green.bgBlack('911 how can i help you?'));
+};

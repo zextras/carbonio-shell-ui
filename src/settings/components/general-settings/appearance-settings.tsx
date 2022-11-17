c96d0b14a83c1117a927b88abb80300141e0a603
Cataldo Mazzilli <cm@zextras.com>
2022-11-17 14:14:23 +0100

    refactor: remove cast

--- src/settings/components/general-settings/appearance-settings.tsx
+++ src/settings/components/general-settings/appearance-settings.tsx
@@ -7,7 +7,12 @@
 import { FormSubSection, Select } from '@zextras/carbonio-design-system';
 import { find } from 'lodash';
 import React, { FC, useCallback, useContext, useMemo } from 'react';
-import { AccountSettings, DarkReaderPropValues } from '../../../../types';
+import {
+	AccountSettings,
+	isZappDarkreaderModeZimletProp,
+	ZappDarkreaderModeZimletProp,
+	ZimletProp
+} from '../../../../types';
 import { ThemeCallbacksContext } from '../../../boot/theme-provider';
 import { DR_VALUES, SHELL_APP_ID } from '../../../constants';
 import { getT } from '../../../store/i18n';
@@ -20,8 +25,10 @@ const AppearanceSettings: FC<{
 	const { setDarkReaderState } = useContext(ThemeCallbacksContext);
 	const currentDRMSetting = useMemo(
 		() =>
-			find(settings.props, { name: 'zappDarkreaderMode', zimlet: SHELL_APP_ID })
-				?._content as DarkReaderPropValues,
+			find<ZimletProp, ZappDarkreaderModeZimletProp>(
+				settings.props,
+				(value): value is ZappDarkreaderModeZimletProp => isZappDarkreaderModeZimletProp(value)
+			)?._content,
 		[settings]
 	);
 	const t = getT();

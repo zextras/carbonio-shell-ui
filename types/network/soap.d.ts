b98d4db174040b7ecf6c08e9b837ee2a9a94d684
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2023-01-31 14:52:35 +0100

    chore: updated type

--- types/network/soap.d.ts
+++ types/network/soap.d.ts
@@ -74,7 +74,7 @@ export type SoapNotify = {
 		m?: Array<unknown>;
 		c?: Array<unknown>;
 		folder?: Array<Partial<SoapFolder>>;
-		link?: Array<Partial<SoapFolder>>;
+		link?: Array<Partial<SoapLink>>;
 		tag?: Array<Partial<Tag>>;
 		mbx: [{ s: number }];
 	};

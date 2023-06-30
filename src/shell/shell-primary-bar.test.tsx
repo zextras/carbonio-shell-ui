d24a771a8654051f342fa9dac11a631ee903eef2
Cataldo Mazzilli <cm@zextras.com>
2023-06-30 17:10:30 +0200

    test: add search view test

--- src/shell/shell-primary-bar.test.tsx
+++ src/shell/shell-primary-bar.test.tsx
@@ -306,4 +306,117 @@ describe('Shell primary bar', () => {
 		expect(screen.queryByText('default mails view')).not.toBeInTheDocument();
 		expect(screen.queryByText('files view')).not.toBeInTheDocument();
 	});
+
+	test('When I move to the search module from any other component the search header selector and search view are switched based on the module from which I originate, also if is not the first access to search module', async () => {
+		const { getByRoleWithIcon, user } = setup(<ShellWrapper />);
+
+		act(() => {
+			useAppStore.getState().setters.addApps([
+				{
+					commit: '',
+					description: 'Mails module',
+					display: 'Mails',
+					icon: 'MailModOutline',
+					js_entrypoint: '',
+					name: 'carbonio-mails-ui',
+					priority: 1,
+					type: 'carbonio',
+					version: '0.0.1'
+				},
+				{
+					commit: '',
+					description: 'Files module',
+					display: 'Files',
+					icon: 'DriveOutline',
+					js_entrypoint: '',
+					name: 'carbonio-files-ui',
+					priority: 2,
+					type: 'carbonio',
+					version: '0.0.1'
+				}
+			]);
+			useAppStore.getState().setters.addRoute({
+				id: 'mails',
+				route: 'mails',
+				position: 1,
+				visible: true,
+				label: 'Mails',
+				primaryBar: 'MailModOutline',
+				appView: MailsView,
+				badge: { show: false },
+				app: 'carbonio-mails-ui'
+			});
+
+			useAppStore.getState().setters.addRoute({
+				id: 'files',
+				route: 'files',
+				position: 2,
+				visible: true,
+				label: 'Files',
+				primaryBar: 'DriveOutline',
+				appView: FilesView,
+				badge: { show: false },
+				app: 'carbonio-files-ui'
+			});
+
+			useAppStore.getState().setters.addSearchView({
+				route: 'files',
+				component: (): JSX.Element => <Text>files search view</Text>,
+				label: 'Files',
+				id: 'files',
+				app: 'carbonio-files-ui',
+				icon: 'DriveOutline',
+				position: 2
+			});
+			useAppStore.getState().setters.addSearchView({
+				route: 'mails',
+				component: (): JSX.Element => <Text>mails search view</Text>,
+				label: 'Mails',
+				id: 'mails',
+				app: 'carbonio-mails-ui',
+				icon: 'MailModOutline',
+				position: 1
+			});
+		});
+
+		const mailsIcon = getByRoleWithIcon('button', { icon: 'MailModOutline' });
+		expect(mailsIcon).toBeVisible();
+		expect(mailsIcon).toBeEnabled();
+		const filesIcon = getByRoleWithIcon('button', { icon: 'DriveOutline' });
+		expect(filesIcon).toBeVisible();
+		expect(filesIcon).toBeEnabled();
+		const searchIcon = getByRoleWithIcon('button', { icon: 'SearchModOutline' });
+		expect(searchIcon).toBeVisible();
+		expect(searchIcon).toBeEnabled();
+
+		expect(screen.getByText('default mails view')).toBeVisible();
+		expect(screen.queryByText('files view')).not.toBeInTheDocument();
+
+		await user.click(filesIcon);
+		expect(screen.getByText('files view')).toBeVisible();
+		expect(screen.queryByText('default mails view')).not.toBeInTheDocument();
+
+		await user.click(searchIcon);
+
+		expect(screen.getByText('files search view')).toBeVisible();
+		expect(within(screen.getByTestId('HeaderModuleSelector')).getByText('Files')).toBeVisible();
+
+		expect(screen.queryByText('mails search view')).not.toBeInTheDocument();
+		expect(screen.queryByText('default mails view')).not.toBeInTheDocument();
+		expect(screen.queryByText('files view')).not.toBeInTheDocument();
+
+		await user.click(mailsIcon);
+		expect(screen.getByText('default mails view')).toBeVisible();
+		expect(screen.queryByText('files view')).not.toBeInTheDocument();
+		expect(screen.queryByText('files search view')).not.toBeInTheDocument();
+
+		await user.click(searchIcon);
+
+		expect(screen.getByText('mails search view')).toBeVisible();
+		expect(within(screen.getByTestId('HeaderModuleSelector')).getByText('Mails')).toBeVisible();
+
+		expect(screen.queryByText('files search view')).not.toBeInTheDocument();
+		expect(screen.queryByText('default mails view')).not.toBeInTheDocument();
+		expect(screen.queryByText('files view')).not.toBeInTheDocument();
+	});
 });

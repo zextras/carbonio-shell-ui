7521165e38bec771d5fd23a2813dac15ac37df33
Cataldo Mazzilli <cm@zextras.com>
2023-07-27 16:54:38 +0200

    chore(deps): remove @babel/plugin-transform-runtime

--- babel.config.jest.js
+++ babel.config.jest.js
@@ -6,5 +6,5 @@
 
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-	plugins: ['@babel/plugin-transform-runtime', 'babel-plugin-styled-components']
+	plugins: ['babel-plugin-styled-components']
 };

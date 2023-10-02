3e8279049fdfe9a9ba43e1bd81ba3b17520418b4
Rodley Orosa <rodley.orosa@zextras.com>
2023-10-02 10:38:45 +0200

    test: change test description
    refs: SHELL-103

--- src/shell/shell-primary-bar.test.tsx
+++ src/shell/shell-primary-bar.test.tsx
@@ -448,7 +448,7 @@ describe('Shell primary bar', () => {
 	});
 
 	describe('Primary Badge', () => {
-		test('should render the counter badge if show and showCount are true and count is greater than or equal to 0', () => {
+		test('the counter badge is shown if count is valued', () => {
 			const primaryBarViews: PrimaryBarView[] = [
 				{
 					id: 'pbv-1',

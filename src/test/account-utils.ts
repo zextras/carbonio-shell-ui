364b5acf308ed83cb64816ce16c93dbe9f6b8b55
Cataldo <cm@zextras.com>
2023-11-14 15:34:40 +0100

    refactor: deduplicate code inside accounts-settings test
    Refs: (#358)

--- src/test/account-utils.ts
+++ src/test/account-utils.ts
@@ -6,7 +6,7 @@
 import { produce } from 'immer';
 
 import { LOGGED_USER } from './constants';
-import { Account } from '../../types';
+import { Account, AccountSettingsPrefs, Identity, IdentityAttrs } from '../../types';
 import { useAccountStore } from '../store/account';
 
 export const mockedAccount: Account = {
@@ -18,10 +18,67 @@ export const mockedAccount: Account = {
 	identities: LOGGED_USER.identities
 };
 
-export function setupAccountStore(account = mockedAccount): void {
+export function setupAccountStore(
+	account = mockedAccount,
+	accountSettingsPrefs: AccountSettingsPrefs = {}
+): void {
 	useAccountStore.setState(
 		produce((state) => {
 			state.account = account;
+			state.settings.attrs.zimbraIdentityMaxNumEntries = 20;
+			state.settings.prefs = accountSettingsPrefs;
 		})
 	);
+}
+
+export function createAccount(name: string, id: string, identities: Array<Identity>): Account {
+	return {
+		name,
+		rights: { targets: [] },
+		signatures: { signature: [] },
+		id,
+		displayName: '',
+		identities: {
+			identity: identities
+		}
+	};
+}
+
+export function createIdentityAttrs(
+	zimbraPrefIdentityId: IdentityAttrs['zimbraPrefIdentityId'],
+	zimbraPrefIdentityName: IdentityAttrs['zimbraPrefIdentityName'],
+	zimbraPrefFromAddress: IdentityAttrs['zimbraPrefFromAddress'],
+	zimbraPrefFromDisplay: IdentityAttrs['zimbraPrefFromDisplay'] = ''
+): IdentityAttrs {
+	const _attrs: IdentityAttrs = {
+		zimbraPrefReplyToEnabled: 'FALSE',
+		zimbraPrefFromDisplay,
+		zimbraPrefFromAddressType: 'sendAs',
+		zimbraPrefIdentityId,
+		zimbraPrefIdentityName,
+		zimbraPrefFromAddress
+	};
+
+	return _attrs;
+}
+
+export function createIdentity(
+	prefs: {
+		zimbraPrefIdentityId: NonNullable<IdentityAttrs['zimbraPrefIdentityId']>;
+		zimbraPrefIdentityName: IdentityAttrs['zimbraPrefIdentityName'];
+		zimbraPrefFromAddress: IdentityAttrs['zimbraPrefFromAddress'];
+		zimbraPrefFromDisplay?: IdentityAttrs['zimbraPrefFromDisplay'];
+	},
+	isDefault: boolean
+): Identity {
+	return {
+		id: prefs.zimbraPrefIdentityId,
+		_attrs: createIdentityAttrs(
+			prefs.zimbraPrefIdentityId,
+			prefs.zimbraPrefIdentityName,
+			prefs.zimbraPrefFromAddress,
+			prefs.zimbraPrefFromDisplay
+		),
+		name: isDefault ? 'DEFAULT' : prefs.zimbraPrefIdentityName
+	};
 }

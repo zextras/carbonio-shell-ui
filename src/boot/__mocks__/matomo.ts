8d0594be791580126dc340d433ac623ac78180ea
Cataldo <cm@zextras.com>
2024-03-13 11:04:38 +0100

    feat: implement matomo tracker
    Co-authored-by: Beatrice Guerra <beatrice.guerra@zextras.com>
    Co-authored-by: Rodley Orosa <rodley.orosa@zextras.com>
    Refs: SHELL-198 (#388)

---
+++ src/boot/__mocks__/matomo.ts
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { MatomoTracker } from '../matomo';
+
+export function initMatomo(): void {
+	// do nothing
+}
+
+export function getMatomoTracker(): MatomoTracker {
+	return {
+		trackEvent: () => undefined,
+		trackPageView: () => undefined,
+		setCustomUrl: () => undefined
+	};
+}

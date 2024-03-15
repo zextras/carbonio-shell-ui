928d05018ec5e59a53acc7b41c82dfc5fcb14c17
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-03-15 17:42:15 +0100

    refactor: remove commented out code

--- src/store/network/utils.ts
+++ src/store/network/utils.ts
@@ -95,30 +95,3 @@ export const handleSync = ({ refresh, notify }: SoapContext): Promise<void> =>
 		}
 		r();
 	});
-// export const noOp = (): void => {
-// 	// eslint-disable-next-line @typescript-eslint/no-use-before-define
-// 	getSoapFetch(SHELL_APP_ID)(
-// 		'NoOp',
-// 		useNetworkStore.getState().pollingInterval === 500
-// 			? { _jsns: JSNS.MAIL, limitToOneBlocked: 1, wait: 1 }
-// 			: { _jsns: JSNS.MAIL }
-// 	);
-// };
-// export const handleSoapContext = ({ notify, refresh, session, change }: SoapContext): void =>
-// 	useNetworkStore.setState({
-// 		session,
-// 		change,
-// 		refresh,
-// 		seq: maxBy(notify, 'seq'),
-// 		noOpTimeout: setTimeout(() => noOp(), pollingInterval),
-// 		notify: map(notify, (notifyItem) => ({
-// 			...notifyItem,
-// 			deleted: notifyItem.deleted?.id?.split(',')
-// 		}))
-// 	});
-// // handleTagSync(_context);
-// // useNetworkStore.setState({
-// // 	noOpTimeout: setTimeout(() => noOp(), pollingInterval),
-// // 	,
-// // 	...res?.Header?.context
-// // });

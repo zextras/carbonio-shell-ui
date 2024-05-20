4164c8d777f26aef83a9e6e29a3ebd24afc54b7f
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-05-20 17:45:02 +0200

    test: update test to consider timezone offset
    Refs: SHELL-217

--- src/settings/components/utils.test.ts
+++ src/settings/components/utils.test.ts
@@ -8,13 +8,18 @@ import { dateToGenTime, genTimeToDate } from './utils';
 describe('dateToGenTime function', () => {
 	it('should return a UTC date with the format YYYYMMDDHHmmss[Z]', () => {
 		const date = new Date(2024, 4, 20, 15, 22, 45);
-		expect(dateToGenTime(date)).toBe('20240520152245Z');
+		const offset = date.getTimezoneOffset();
+		const dateUTC = new Date(date.getTime() - offset * 60 * 1000);
+		expect(dateToGenTime(dateUTC)).toBe('20240520152245Z');
 	});
 });
 
 describe('genTimeToDate function', () => {
 	it('should return a date starting from a valid string with format YYYYMMDDHHmmss[Z]', () => {
 		const dateStr = '20240520152245Z';
-		expect(genTimeToDate(dateStr)).toEqual(new Date(2024, 4, 20, 15, 22, 45, 0));
+		const date = new Date(2024, 4, 20, 15, 22, 45, 0);
+		const offset = date.getTimezoneOffset();
+		const dateUTC = new Date(date.getTime() - offset * 60 * 1000);
+		expect(genTimeToDate(dateStr)).toEqual(dateUTC);
 	});
 });

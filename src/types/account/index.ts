bc408401e04f14dfd51a1da920fdab14db6a92e4
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-06-12 17:41:21 +0200

    fix: add a message for the deprecated annotation

--- src/types/account/index.ts
+++ src/types/account/index.ts
@@ -85,7 +85,7 @@ export interface AccountSettingsPrefs {
 	zimbraPrefDelegatedSendSaveTarget?: (typeof DELEGATED_SEND_SAVE_TARGET)[number];
 	/**
 	 * The timezone preference is going to be removed, because now we rely on the system timezone.
-	 * @deprecated
+	 * @deprecated rely on the system timezone instead
 	 */
 	zimbraPrefTimeZoneId?: string;
 	[key: string]: string | number | Array<string | number> | undefined;

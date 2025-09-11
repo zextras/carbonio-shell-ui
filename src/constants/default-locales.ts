73723d361eed93974d132dee8063719c532fbb41
Cataldo Mazzilli <cm@zextras.com>
2025-09-11 11:43:42 +0200

    fix: wip

--- src/constants/default-locales.ts
+++ src/constants/default-locales.ts
@@ -3,13 +3,154 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import type { LocaleValue } from './locales';
+
+type LocaleValue = {
+	value: string;
+	name: string;
+	tinymceLocale: string | undefined;
+	labelKey: string;
+	labelDefaultValue: string;
+};
 
 export const DEFAULT_LOCALES: Record<string, LocaleValue> = {
+	zh_CN: {
+		name: '中文 (中国)',
+		value: 'zh_CN',
+		tinymceLocale: 'zh-Hans',
+		labelKey: 'locale.label_chinese',
+		labelDefaultValue: 'Chinese (China) - {{value}}'
+	},
+	nl: {
+		name: 'Nederlands',
+		value: 'nl',
+		tinymceLocale: 'nl',
+		labelKey: 'locale.label_dutch',
+		labelDefaultValue: 'Dutch - {{value}}'
+	},
+	en: {
+		name: 'English',
+		value: 'en',
+		tinymceLocale: 'en',
+		labelKey: 'locale.label_english',
+		labelDefaultValue: 'English - {{value}}'
+	},
+	de: {
+		name: 'Deutsch',
+		value: 'de',
+		tinymceLocale: 'de',
+		labelKey: 'locale.label_german',
+		labelDefaultValue: 'German - {{value}}'
+	},
+	hi: {
+		name: 'हिंदी',
+		value: 'hi',
+		tinymceLocale: 'hi',
+		labelKey: 'locale.label_hindi',
+		labelDefaultValue: 'Hindi - {{value}}'
+	},
+	hu: {
+		name: 'Magyar',
+		value: 'hu',
+		tinymceLocale: 'hu_HU',
+		labelKey: 'locale.label_hungarian',
+		labelDefaultValue: 'Hungarian - {{value}}'
+	},
 	it: {
+		name: 'italiano',
 		value: 'it',
-		name: '',
-		tinymceLocale: '',
-		dateFnsLocale: ''
+		tinymceLocale: 'it',
+		labelKey: 'locale.label_italian',
+		labelDefaultValue: 'Italian - {{value}}'
+	},
+	ja: {
+		name: '日本語',
+		value: 'ja',
+		tinymceLocale: 'ja',
+		labelKey: 'locale.label_japanese',
+		labelDefaultValue: 'Japanese - {{value}}'
+	},
+	pt: {
+		name: 'português',
+		value: 'pt',
+		tinymceLocale: 'pt_BR',
+		labelKey: 'locale.label_portuguese',
+		labelDefaultValue: 'Portuguese - {{value}}'
+	},
+	pl: {
+		name: 'polski',
+		value: 'pl',
+		tinymceLocale: 'pl',
+		labelKey: 'locale.label_polish',
+		labelDefaultValue: 'Polish - {{value}}'
+	},
+	ro: {
+		name: 'română',
+		value: 'ro',
+		tinymceLocale: 'ro',
+		labelKey: 'locale.label_romanian',
+		labelDefaultValue: 'Romanian - {{value}}'
+	},
+	ru: {
+		name: 'русский',
+		value: 'ru',
+		tinymceLocale: 'ru',
+		labelKey: 'locale.label_russian',
+		labelDefaultValue: 'Russian - {{value}}'
+	},
+	es: {
+		name: 'español',
+		value: 'es',
+		tinymceLocale: 'es',
+		labelKey: 'locale.label_spanish',
+		labelDefaultValue: 'Spanish - {{value}}'
+	},
+	th: {
+		name: 'ไทย',
+		value: 'th',
+		tinymceLocale: 'th_TH',
+		labelKey: 'locale.label_thai',
+		labelDefaultValue: 'Thai - {{value}}'
+	},
+	tr: {
+		name: 'Türkçe',
+		value: 'tr',
+		tinymceLocale: 'tr',
+		labelKey: 'locale.label_turkish',
+		labelDefaultValue: 'Turkish - {{value}}'
+	},
+	fr: {
+		name: 'français',
+		value: 'fr',
+		tinymceLocale: 'fr_FR',
+		labelKey: 'locale.label_french',
+		labelDefaultValue: 'French - {{value}}'
+	},
+	vi: {
+		name: 'Tiếng Việt',
+		value: 'vi',
+		tinymceLocale: 'vi',
+		labelKey: 'locale.label_vietnamese',
+		labelDefaultValue: 'Vietnamese - {{value}}'
+	},
+	ky: {
+		name: 'Кыргызча',
+		value: 'ky',
+		tinymceLocale: 'ky',
+		labelKey: 'locale.label_kyrgyz',
+		labelDefaultValue: 'Kyrgyz - {{value}}'
+	},
+	bs: {
+		name: 'Bosanski',
+		value: 'bs',
+		tinymceLocale: 'bs',
+		labelKey: 'locale.label_bosnian',
+		labelDefaultValue: 'Bosnian - {{value}}'
+	},
+	sl: {
+		name: 'Slovenščina',
+		value: 'sl',
+		tinymceLocale: 'sl_SI',
+		labelKey: 'locale.label_slovenian',
+		labelDefaultValue: 'Slovenian - {{value}}'
 	}
 };
--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -7,12 +7,7 @@
 import type { Locale } from 'date-fns';
 import type { TFunction } from 'i18next';
 
-export type LocaleValue = {
-	value: string;
-	name: string;
-	tinymceLocale: string;
-	dateFnsLocale: string | undefined;
-};
+import { DEFAULT_LOCALES } from './default-locales';
 
 export type LocaleDescriptor = {
 	name: string;
@@ -27,8 +22,131 @@ export type LocaleDescriptor = {
 	tinymceLocale?: string;
 };
 
+export const DATE_FNS_LOCALE: Record<
+	string,
+	{ key?: string; localeImportPath: () => Promise<Locale> } | undefined
+> = {
+	zh_CN: {
+		key: 'zh-CN',
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "zh-CN" */ import('date-fns/locale/zh-CN').then(
+				({ zhCN }) => zhCN
+			)
+	},
+	nl: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "nl" */ import('date-fns/locale/nl').then(
+				({ nl }) => nl
+			)
+	},
+	en: {
+		key: 'en-US',
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "en-US" */ import('date-fns/locale/en-US').then(
+				({ enUS }) => enUS
+			)
+	},
+	de: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "de" */ import('date-fns/locale/de').then(
+				({ de }) => de
+			)
+	},
+	hi: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "hi" */ import('date-fns/locale/hi').then(
+				({ hi }) => hi
+			)
+	},
+	hu: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "hu" */ import('date-fns/locale/hu').then(
+				({ hu }) => hu
+			)
+	},
+	it: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "it" */ import('date-fns/locale/it').then(
+				({ it }) => it
+			)
+	},
+	ja: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "ja" */ import('date-fns/locale/ja').then(
+				({ ja }) => ja
+			)
+	},
+	pt: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "pt" */ import('date-fns/locale/pt').then(
+				({ pt }) => pt
+			)
+	},
+	pl: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "pl" */ import('date-fns/locale/pl').then(
+				({ pl }) => pl
+			)
+	},
+	ro: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "ro" */ import('date-fns/locale/ro').then(
+				({ ro }) => ro
+			)
+	},
+	ru: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "ru" */ import('date-fns/locale/ru').then(
+				({ ru }) => ru
+			)
+	},
+	es: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "es" */ import('date-fns/locale/es').then(
+				({ es }) => es
+			)
+	},
+	th: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "th" */ import('date-fns/locale/th').then(
+				({ th }) => th
+			)
+	},
+	tr: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "tr" */ import('date-fns/locale/tr').then(
+				({ tr }) => tr
+			)
+	},
+	fr: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "fr" */ import('date-fns/locale/fr').then(
+				({ fr }) => fr
+			)
+	},
+	vi: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "vi" */ import('date-fns/locale/vi').then(
+				({ vi }) => vi
+			)
+	},
+	ky: undefined,
+	bs: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "bs" */ import('date-fns/locale/bs').then(
+				({ bs }) => bs
+			)
+	},
+	sl: {
+		localeImportPath: () =>
+			/* webpackMode: "lazy", webpackChunkName: "sl" */ import('date-fns/locale/sl').then(
+				({ sl }) => sl
+			)
+	}
+} as const;
+
 const mapCacheToSupportedLocales = (
-	cache: Record<string, LocaleValue>
+	cache: typeof DEFAULT_LOCALES
 ): Record<string, LocaleDescriptor> =>
 	Object.values(cache).reduce(
 		(previousValue, currentValue) => {
@@ -37,338 +155,23 @@ const mapCacheToSupportedLocales = (
 				value: currentValue.value,
 				name: currentValue.name,
 				tinymceLocale: currentValue.tinymceLocale,
-				dateFnsLocale:
-					typeof currentValue.dateFnsLocale === 'string'
-						? {
-								localeImportPath: () =>
-									/* webpackMode: "lazy" */ import(
-										`date-fns/locale/${currentValue.dateFnsLocale}`
-									).then(
-										(m) =>
-											m[
-												(currentValue.dateFnsLocale as string).replace('-', '')
-											] as unknown as Locale
-									)
-							}
-						: undefined
+				dateFnsLocale: DATE_FNS_LOCALE[currentValue.value]
 			};
 			return previousValue;
 		},
 		{} as Record<string, LocaleDescriptor>
 	);
 
-export const SUPPORTED_LOCALES: Record<string, LocaleDescriptor> = {
-	zh_CN: {
-		name: '中文 (中国)',
-		value: 'zh_CN',
-		dateFnsLocale: {
-			key: 'zh-CN',
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/zh-CN').then(({ zhCN }) => zhCN)
-		},
-		tinymceLocale: 'zh-Hans'
-	},
-	nl: {
-		name: 'Nederlands',
-		value: 'nl',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/nl').then((value) => value.nl)
-		}
-	},
-	en: {
-		name: 'English',
-		value: 'en',
-		dateFnsLocale: {
-			key: 'en-US',
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/en-US').then(({ enUS }) => enUS)
-		}
-	},
-	de: {
-		name: 'Deutsch',
-		value: 'de',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/de').then(({ de }) => de)
-		}
-	},
-	hi: {
-		name: 'हिंदी',
-		value: 'hi',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/hi').then(({ hi }) => hi)
-		}
-	},
-	hu: {
-		name: 'Magyar',
-		value: 'hu',
-		tinymceLocale: 'hu_HU',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/hu').then(({ hu }) => hu)
-		}
-	},
-	it: {
-		name: 'italiano',
-		value: 'it',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/it').then(({ it }) => it)
-		}
-	},
-	ja: {
-		name: '日本語',
-		value: 'ja',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/ja').then(({ ja }) => ja)
-		}
-	},
+export const SUPPORTED_LOCALES: Record<string, LocaleDescriptor> =
+	mapCacheToSupportedLocales(DEFAULT_LOCALES);
 
-	pt: {
-		name: 'português',
-		value: 'pt',
-		tinymceLocale: 'pt_BR',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/pt').then(({ pt }) => pt)
-		}
-	},
-	pl: {
-		name: 'polski',
-		value: 'pl',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/pl').then(({ pl }) => pl)
-		}
-	},
-
-	ro: {
-		name: 'română',
-		value: 'ro',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/ro').then(({ ro }) => ro)
-		}
-	},
-	ru: {
-		name: 'русский',
-		value: 'ru',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/ru').then(({ ru }) => ru)
-		}
-	},
-	es: {
-		name: 'español',
-		value: 'es',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/es').then(({ es }) => es)
-		}
-	},
-	th: {
-		name: 'ไทย',
-		value: 'th',
-		tinymceLocale: 'th_TH',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/th').then(({ th }) => th)
-		}
-	},
-	tr: {
-		name: 'Türkçe',
-		value: 'tr',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/tr').then(({ tr }) => tr)
-		}
-	},
-	fr: {
-		name: 'français',
-		value: 'fr',
-		tinymceLocale: 'fr_FR',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/fr').then(({ fr }) => fr)
-		}
-	},
-	vi: {
-		name: 'Tiếng Việt',
-		value: 'vi',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/vi').then(({ vi }) => vi)
-		}
-	},
-	ky: {
-		name: 'Кыргызча',
-		value: 'ky',
-		dateFnsLocale: undefined
-	},
-	bs: {
-		name: 'Bosanski',
-		value: 'bs',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/bs').then(({ bs }) => bs)
-		}
-	},
-	sl: {
-		name: 'Slovenščina',
-		value: 'sl',
-		tinymceLocale: 'sl_SI',
-		dateFnsLocale: {
-			localeImportPath: () =>
-				/* webpackMode: "lazy" */ import('date-fns/locale/sl').then(({ sl }) => sl)
-		}
-	}
-} as const;
-
-export type LocaleDescriptorWithLabels = LocaleDescriptor & {
+export type LocaleDescriptorWithLabels = {
 	label: string;
+	value: string;
 };
-export const localeList = (t: TFunction): Array<LocaleDescriptorWithLabels> => [
-	{
-		...SUPPORTED_LOCALES.zh_CN,
-		label: t('locale.label_chinese', {
-			value: SUPPORTED_LOCALES.zh_CN.name,
-			defaultValue: 'Chinese (China) - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.nl,
-		label: t('locale.label_dutch', {
-			value: SUPPORTED_LOCALES.nl.name,
-			defaultValue: 'Dutch - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.en,
-		label: t('locale.label_english', {
-			value: SUPPORTED_LOCALES.en.name,
-			defaultValue: 'English - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.de,
-		label: t('locale.label_german', {
-			value: SUPPORTED_LOCALES.de.name,
-			defaultValue: 'German - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.hi,
-		label: t('locale.label_hindi', {
-			value: SUPPORTED_LOCALES.hi.name,
-			defaultValue: 'Hindi - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.hu,
-		label: t('locale.label_hungarian', {
-			value: SUPPORTED_LOCALES.hu.name,
-			defaultValue: 'Hungarian - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.it,
-		label: t('locale.label_italian', {
-			value: SUPPORTED_LOCALES.it.name,
-			defaultValue: 'Italian - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.ja,
-		label: t('locale.label_japanese', {
-			value: SUPPORTED_LOCALES.ja.name,
-			defaultValue: 'Japanese - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.pt,
-		label: t('locale.label_portuguese', {
-			value: SUPPORTED_LOCALES.pt.name,
-			defaultValue: 'Portuguese - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.pl,
-		label: t('locale.label_polish', {
-			value: SUPPORTED_LOCALES.pl.name,
-			defaultValue: 'Polish - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.ro,
-		label: t('locale.label_romanian', {
-			value: SUPPORTED_LOCALES.ro.name,
-			defaultValue: 'Romanian - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.ru,
-		label: t('locale.label_russian', {
-			value: SUPPORTED_LOCALES.ru.name,
-			defaultValue: 'Russian - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.es,
-		label: t('locale.label_spanish', {
-			value: SUPPORTED_LOCALES.es.name,
-			defaultValue: 'Spanish - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.th,
-		label: t('locale.label_thai', {
-			value: SUPPORTED_LOCALES.th.name,
-			defaultValue: 'Thai - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.tr,
-		label: t('locale.label_turkish', {
-			value: SUPPORTED_LOCALES.tr.name,
-			defaultValue: 'Turkish - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.fr,
-		label: t('locale.label_french', {
-			value: SUPPORTED_LOCALES.fr.name,
-			defaultValue: 'French - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.vi,
-		label: t('locale.label_vietnamese', {
-			value: SUPPORTED_LOCALES.vi.name,
-			defaultValue: 'Vietnamese - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.ky,
-		label: t('locale.label_kyrgyz', {
-			value: SUPPORTED_LOCALES.ky.name,
-			defaultValue: 'Kyrgyz - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.bs,
-		label: t('locale.label_bosnian', {
-			value: SUPPORTED_LOCALES.bs.name,
-			defaultValue: 'Bosnian - {{value}}'
-		})
-	},
-	{
-		...SUPPORTED_LOCALES.sl,
-		label: t('locale.label_slovenian', {
-			value: SUPPORTED_LOCALES.sl.name,
-			defaultValue: 'Slovenian - {{value}}'
-		})
-	}
-];
+
+export const localeList = (t: TFunction): Array<LocaleDescriptorWithLabels> =>
+	Object.values(DEFAULT_LOCALES).map((locale) => ({
+		value: locale.value,
+		label: t(locale.labelKey, { value: locale.name, defaultValue: locale.labelDefaultValue })
+	}));

c55aa26c20a7078f3cc4677ab6cba91f1ceccd03
Cataldo Mazzilli <cm@zextras.com>
2026-04-24 09:58:41 +0200

    ci: update jenkins-lib-ui version and add semantic-release configuration (#784)

---
+++ release.config.mjs
@@ -0,0 +1,77 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+	branches: ['devel'],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'conventionalcommits',
+				releaseRules: [
+					// enable release also for refactor and build commits
+					{ type: 'refactor', release: 'patch' },
+					{ type: 'build', release: 'patch' },
+					{ type: 'ci', release: 'patch' },
+					{ type: 'perf', release: 'patch' }
+				]
+			}
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'conventionalcommits',
+				presetConfig: {
+					// see https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.2.0/README.md#types
+					types: [
+						{
+							type: 'feat',
+							section: 'Features',
+							hidden: false
+						},
+						{
+							type: 'fix',
+							section: 'Bug Fixes',
+							hidden: false
+						},
+						{
+							type: 'refactor',
+							section: 'Other changes',
+							hidden: false
+						},
+						{
+							type: 'perf',
+							section: 'Other changes',
+							hidden: false
+						},
+						{
+							type: 'build',
+							section: 'Other changes',
+							hidden: false
+						},
+						{
+							type: 'ci',
+							section: 'Other changes',
+							hidden: false
+						}
+					]
+				}
+			}
+		],
+		'@semantic-release/npm',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]'
+			}
+		],
+		'@semantic-release/github'
+	]
+};

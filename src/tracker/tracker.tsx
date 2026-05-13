d217952fb211c7feaf4f757331e823f4453d118f
Cataldo Mazzilli <cm@zextras.com>
2026-05-13 12:44:31 +0200

    refactor(tracker): remove tracker logic from Loader and consolidate in TrackerProvider
    Refs: CO-3507 (#800)

--- src/tracker/tracker.tsx
+++ src/tracker/tracker.tsx
@@ -3,13 +3,12 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 
-import type { CaptureOptions, Properties } from 'posthog-js';
+import type { CaptureOptions, PostHogInterface, Properties } from 'posthog-js';
 import { usePostHog } from 'posthog-js/react';
 
 import { useAccountStore } from '../store/account';
-import { useIsCarbonioCE } from '../store/login/hooks';
 import { getCurrentLocationHost } from '../utils/utils';
 
 export interface Tracker {
@@ -34,44 +33,37 @@ const arrayBufferToBase64 = (buffer: ArrayBuffer): string => {
 	return window.btoa(binary);
 };
 
+export const isLocalHost = (): boolean => {
+	const host = getCurrentLocationHost();
+	return host.includes('127.0.0.1') || host.includes('localhost');
+};
+
+export const identifyCurrentUser = (postHog: PostHogInterface): void => {
+	if (isLocalHost()) {
+		return;
+	}
+	const { account } = useAccountStore.getState();
+	if (account?.id) {
+		hashToSHA256(account.id).then((arrayBuffer) => {
+			const hashUserId = arrayBufferToBase64(arrayBuffer);
+			postHog.identify(hashUserId);
+		});
+	}
+};
+
 export const useTracker = (): Tracker => {
 	const postHog = usePostHog();
-	const isCarbonioCE = useIsCarbonioCE();
-	const [isOptedIn, setIsOptedIn] = useState(postHog.has_opted_in_capturing());
-
-	useEffect(() => {
-		if (isCarbonioCE !== undefined) {
-			postHog.setPersonProperties({ is_ce: isCarbonioCE });
-		}
-	}, [isCarbonioCE, postHog]);
-
-	useEffect(() => {
-		const newValue = !isCarbonioCE || !isOptedIn;
-		if (postHog.config.disable_surveys !== newValue && isCarbonioCE !== undefined) {
-			postHog.set_config({ disable_surveys: newValue });
-		}
-	}, [isCarbonioCE, isOptedIn, postHog]);
 
 	const enableTracker = useCallback(
 		(enable: boolean) => {
-			if (
-				!getCurrentLocationHost().includes('127.0.0.1') &&
-				!getCurrentLocationHost().includes('localhost')
-			) {
-				if (enable) {
-					const { account } = useAccountStore.getState();
-					if (account?.id) {
-						hashToSHA256(account.id).then((arrayBuffer) => {
-							const hashUserId = arrayBufferToBase64(arrayBuffer);
-							postHog.identify(hashUserId);
-						});
-					}
-					postHog.opt_in_capturing();
-					setIsOptedIn(true);
-				} else {
-					postHog.opt_out_capturing();
-					setIsOptedIn(false);
-				}
+			if (isLocalHost()) {
+				return;
+			}
+			if (enable) {
+				identifyCurrentUser(postHog);
+				postHog.opt_in_capturing();
+			} else {
+				postHog.opt_out_capturing();
 			}
 		},
 		[postHog]
